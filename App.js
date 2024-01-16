4cd1d45b84dffd7f32a2da9c249e72cb4ac0e9bf
Vaibhav Thobbi <vaibhav.thobbi@expleogroup.com>
2024-01-16 12:22:33 +0530

    installedReactandReactDOM

--- App.js
+++ App.js
@@ -13,11 +13,14 @@
  *
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
+      React.createElement("h1", {}, "I'm an h1 tag of the div1"),
+      React.createElement("h2", {}, "I'm an h2 tag of the div1"),
     ]),
     React.createElement("div", { id: "child2" }, [
       React.createElement("h1", {}, "I'm an h1 tag"),
