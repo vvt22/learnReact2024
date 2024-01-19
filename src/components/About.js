6ac688702d57bfadba1f5ba9c741900ab474b73e
Vaibhav Thobbi <vaibhav.thobbi@expleogroup.com>
2024-01-19 14:19:40 +0530

    olderwayCLASSbased

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,26 @@
-const About = () => {
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    //console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    //console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    //console.log("Parent Render");
     return (
       <div>
-        <h1>About</h1>
+        <h1>About Class Component</h1>
         <h2>This is Namaste React Web Series</h2>
+        <UserClass name={"First"} location={"Dehradun Class"} />
       </div>
     );
-  };
-  
-  export default About;
+  }
+}
