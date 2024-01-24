f5ac5ef650d22eec6d9ecd92b18e8a571d31e036
Vaibhav Thobbi <vaibhav.thobbi@expleogroup.com>
2024-01-24 21:04:02 +0530

    customHook

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import resList from "../../utils/mockdata";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestraunt] = useState([]);
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import useRestaurantMenu from "../../utils/useRestaurant";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../../utils/constants";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
   const { resId } = useParams();
-
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
