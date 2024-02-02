04715f2de8c67c4021bca26b10957d8516bfc77f
Vaibhav Thobbi <vaibhav.thobbi@expleogroup.com>
2024-02-02 20:41:55 +0530

    accordianheader

--- src/RestaurantCategory.js
+++ src/RestaurantCategory.js
@@ -2,7 +2,12 @@ const RestaurantCategory =({data})=>{
     console.log(data);
     return(<div>
         {/* Header */}
-        ResCategory
+        <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between">
+        <span className="font-bold text-sm">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span >⬇️</span>
+        </div>
         {/* Accordian */}
     </div>);
 };
