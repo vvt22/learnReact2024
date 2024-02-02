1e934d310783a0026a9661d7c9347992de733229
Vaibhav Thobbi <vaibhav.thobbi@expleogroup.com>
2024-02-02 20:31:41 +0530

    gettingdataforCAT

---
+++ src/RestaurantCategory.js
@@ -0,0 +1,9 @@
+const RestaurantCategory =({data})=>{
+    console.log(data);
+    return(<div>
+        {/* Header */}
+        ResCategory
+        {/* Accordian */}
+    </div>);
+};
+export default RestaurantCategory;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import Shimmer from "./Shimmer";
 import useRestaurantMenu from "../../utils/useRestaurant";
 import { useParams } from "react-router-dom";
+import RestaurantCategory from "../RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -14,12 +15,16 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
-  console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+  //console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
   //adding category
   const categories =
-  resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>{c.card?.card?.["@type"]==
-  "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"})
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  //console.log(categories);
 
   return (
     <div className="text-center ">
@@ -28,6 +33,7 @@ const RestaurantMenu = () => {
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
       {/* categories accordian  */}
+      {categories.map((category)=><RestaurantCategory data={category?.card?.card}/>)}
     </div>
   );
 };
